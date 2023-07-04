a841a4d4d7f7bbd10ef5695eec37cf173313eacd
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-04 20:01:47 +0900

    feat: db connection 구현

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 //packages
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect('');
-
+mongoose.connect(process.env.MONGODB);
 const db = mongoose.connection;
 
 db.on('connected', () => console.log('connecting DB success'));
+db.on('disconnected', () => console.warn('disconnect'));
 db.on('error', err => console.error(err));
+db.on('reconnectedFailed', () => console.error('reconnect failed'));
 
 app.listen(port, () => {
 	console.log(`connecting to ${port}`);
