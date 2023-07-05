c2974b44ba898116474448b27f72c2b4c8e3e9f3
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-05 19:25:07 +0900

    Feat: routers index.js 작성

---
+++ src/routers/index.js
@@ -0,0 +1,6 @@
+const { userRouter } = require('./UserRouter');
+const { GuestOrderHistoryRouter } = require('./GuestOrderHistoryRouter');
+const { OrderRouter } = require('./OrderRouter');
+const { ProductRouter } = require('./ProductRouter');
+
+export { userRouter, GuestOrderHistoryRouter, OrderRouter, ProductRouter };
