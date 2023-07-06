a28996297259ec93603f28c97724e2c06d420118
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-06 15:58:52 +0900

    Feat: 유저 컨트롤러 정보수정,탈퇴, 회원가입 양식 작성

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,12 +1,53 @@
 const { User } = require('../db/models');
 
 const UserController = {
-	userValidation: async (req, res, next) => {
+	updateUser: async (req, res, next) => {
+		//헤더에 토큰값 체크
+		const { email, name, password, address } = await req.body; //address:Object[] = required:false
+
 		try {
+			await User.create({ email, name, password, address });
 		} catch (err) {
 			next(err);
 		}
+
+		return res.status(200).json({
+			message: '회원 정보 수정 성공',
+		});
 	},
+
+	deleteUser: async (req, res, next) => {
+		//헤더에 토큰값 체크
+		const { email } = await req.body; //필수 : validation check
+		try {
+			await User.updateOne({ email }).set({ deletedAt: true });
+		} catch (err) {
+			next(err);
+		}
+		return res.status(200).json({
+			message: '회원 탈퇴 성공',
+		});
+	},
+
+	userSignup: async (req, res, next) => {
+		const { email, name, password } = await req.body; //필수 : 이메일 중복체크, address=required:false
+
+		try {
+			await User.create({
+				email,
+				name,
+				password,
+			});
+		} catch (err) {
+			next(err);
+		}
+
+		return res.status(201).json({
+			msg: '가입 완료',
+		});
+	},
+
+	userGetInformation: (req, res, next) => {},
 };
 
 module.exports = UserController;
