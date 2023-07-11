d05c7c555dacfcb57388631852d207b8c595c155
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-12 01:16:37 +0900

    Feat: signup searchedEmail event추가

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -4,9 +4,11 @@ const userName = document.querySelector('.form-name');
 const pw = document.querySelector('.form-pw');
 const pwCheck = document.querySelector('.form-pwCheck');
 const form = document.querySelector('form');
+const emailOverlapCheck = document.querySelector('.signup-email > button');
 
-const handleSubmit = e => {
+const signupEvent = e => {
 	e.preventDefault();
+
 	if (pw.value !== pwCheck.value) {
 		// 비밀번호가 일치하지 않을때?
 		alert('비밀번호가 일치하지 않습니다.');
@@ -35,26 +37,29 @@ const handleSubmit = e => {
 			.catch(err => {
 				alert(err);
 			});
-
-		// console.log('test');
-		// axios({
-		// 	method: 'post',
-		// 	url: '/api/users/signup', // path
-		// 	data: {
-		// 		email: email.value, // 아이디
-		// 		name: userName.value, // 이름
-		// 		password: pw.value, // 비밀번호
-		// 	},
-		// }).then(res => {
-		// 	if (res.status === 201) {
-		// 		alert(`성공적으로 회원가입이 되었습니다.`);
-		// 		// 로그인 페이지 이동
-		// 		window.location.href = '/signIn';
-		// 	}
-		// });
-		// .catch(err => {
-		// 	alert(err);
-		// });
 	}
 };
-form.addEventListener('submit', handleSubmit); // 가입하기 버튼을 클릭하였을때 handleSubmit 함수가 동작
+
+const searchedEmail = () => {
+	// console.log(email.value);
+
+	fetch(`/api/users/signup/${email.value}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then(res => {
+			if (res.ok) {
+				alert(`사용 가능한 이메일입니다.`);
+			} else {
+				throw new Error('중복된 이메일입니다.');
+			}
+		})
+		.catch(err => {
+			alert(err);
+		});
+};
+
+form.addEventListener('submit', signupEvent); // 가입하기 버튼을 클릭하였을때 handleSubmit 함수가 동작
+emailOverlapCheck.addEventListener('click', searchedEmail); // 가입하기 버튼을 클릭하였을때 handleSubmit 함수가 동작
