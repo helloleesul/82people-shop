c3d4387f1128216c0a80bee2cad6ee52644f3976
[SW5] 양우석 <aoskdls@gmail.com>
2023-07-12 00:44:34 +0900

    Feat: axios에서 fetch로 변경

--- src/views/SignUp/SignUp.js
+++ src/views/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+// import axios from 'axios';
 const email = document.querySelector('.form-email');
 const userName = document.querySelector('.form-name');
 const pw = document.querySelector('.form-pw');
@@ -11,26 +11,50 @@ const handleSubmit = e => {
 		// 비밀번호가 일치하지 않을때?
 		alert('비밀번호가 일치하지 않습니다.');
 	} else {
-		axios
-			.post({
-				// method: 'post',
-				url: '/api/users/signup', // path
-				data: {
-					email: email.value, // 아이디
-					name: userName.value, // 이름
-					password: pw.value, // 비밀번호
-				},
-			})
+		fetch('/api/users/signup', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: email.value,
+				name: userName.value, // 이름
+				password: pw.value, // 비밀번호
+			}),
+		})
 			.then(res => {
-				if (res.status === 201) {
+				if (res.ok) {
 					alert(`성공적으로 회원가입이 되었습니다.`);
 					// 로그인 페이지 이동
+					console.log(res);
 					window.location.href = '/signIn';
+				} else {
+					throw new Error('회원가입 실패');
 				}
 			})
 			.catch(err => {
 				alert(err);
 			});
+
+		// console.log('test');
+		// axios({
+		// 	method: 'post',
+		// 	url: '/api/users/signup', // path
+		// 	data: {
+		// 		email: email.value, // 아이디
+		// 		name: userName.value, // 이름
+		// 		password: pw.value, // 비밀번호
+		// 	},
+		// }).then(res => {
+		// 	if (res.status === 201) {
+		// 		alert(`성공적으로 회원가입이 되었습니다.`);
+		// 		// 로그인 페이지 이동
+		// 		window.location.href = '/signIn';
+		// 	}
+		// });
+		// .catch(err => {
+		// 	alert(err);
+		// });
 	}
 };
 form.addEventListener('submit', handleSubmit); // 가입하기 버튼을 클릭하였을때 handleSubmit 함수가 동작
