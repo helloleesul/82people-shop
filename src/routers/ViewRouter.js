b07181a34dbe21c54deeac5728fbd5b86a90191a
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-12 22:35:27 +0900

    Feat: view라우터 수정

--- src/routers/ViewRouter.js
+++ src/routers/ViewRouter.js
@@ -3,29 +3,35 @@ const ViewService = require('../services/ViewService');
 const ViewRouter = express.Router();
 
 //root
-ViewRouter.use('/', ViewService.serveStatic('Home'));
+ViewRouter.use('/', ViewService.serveStatic('Home')); //메인화면
 
 //product
-// ViewRouter.use('/products', ViewService.serveStatic()); // 메인 화면
 // ViewRouter.use('/products/:productId'); //제품 상세 보기
 // ViewRouter.use('/products/category'); //카테고리별 제품
 
 //user
 // ViewRouter.use('/users');
 ViewRouter.use('/signup', ViewService.serveStatic('SignUp')); //회원가입 페이지
-ViewRouter.use('/my-page', ViewService.serveStatic('MemberInfo')); //마이페이지
+ViewRouter.use('/myPage', ViewService.serveStatic('MemberInfo')); //마이페이지
 ViewRouter.use(
-	'/my-page/delete-membership',
+	'/myPage/delete-membership',
 	ViewService.serveStatic('MemberDelete')
 ); //회원 탈퇴
+ViewRouter.use(
+	'/myPage/orders/history',
+	ViewService.serveStatic('OrderHistory')
+); // 회원 주문 조회 페이지
+ViewRouter.use(
+	'/myPage/orders/history/:orderId',
+	ViewService.serveStatic('OrderDetail')
+); // 주문 상세 조회 페이지
 
 //order
 ViewRouter.use('/cart', ViewService.serveStatic('Cart')); // 장바구니 페이지
 ViewRouter.use('/orders', ViewService.serveStatic('Order')); // 주문 페이지
 ViewRouter.use('/orders/complete', ViewService.serveStatic('OrderComplete')); //주 문 완료 페이지
-ViewRouter.use('/orders/history', ViewService.serveStatic('OrderHistory')); // 회원 주문 조회 페이지
 ViewRouter.use(
-	'/orders/history/:orderId',
+	'/guest/orders/history/:orderId',
 	ViewService.serveStatic('OrderDetail')
 ); // 주문 상세 조회 페이지
 
