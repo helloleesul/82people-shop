109e832d7d335b77dcce3d6622b5da6703956425
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-13 13:52:08 +0900

    Feat: product details 수정

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -38,7 +38,7 @@ const getProducts = newProduct => {
 	// ${newProduct.imageURL}
 	return `<li>
     <a class='icon-img'
-    href='/products?id=${newProduct._id}' target='_self' id='bring-list'>
+    href='/products?productId=${newProduct._id}' target='_self' id='bring-list'>
 	<div class="icon-img">
     <img class='icon-img'
     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE2O8ul5WH3_NdITW8bd4_7gpd6CoSVo1qUY2IHTfE8A&s' alt="product-item"/>
