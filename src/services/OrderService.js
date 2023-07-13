59645c66d400fe49d85adbf59be66d2e2af5bc4d
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-14 01:08:03 +0900

    Feat: 비회원 주문 추가

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -13,7 +13,7 @@ const OrderService = {
 		address,
 		detailAddress,
 		shippingRequest,
-		shippingPrice,
+		shippingPrice
 	) => {
 		const totalProductsPrice = purchase.reduce((acc, product) => {
 			return acc + product.price * product.orderAmount;
@@ -42,7 +42,12 @@ const OrderService = {
 		purchase.map(async product => {
 			await Product.updateOne(
 				{ _id: product.productId },
-				{ $inc: { salesAmount: product.orderAmount, currentAmount: -product.orderAmount }, }
+				{
+					$inc: {
+						salesAmount: product.orderAmount,
+						currentAmount: -product.orderAmount,
+					},
+				}
 			);
 		});
 
@@ -61,25 +66,39 @@ const OrderService = {
 
 	// [회원] 주문 내역 전체 조회
 	checkOrderHistory: async email => {
-        const orderHistory = await Order.find(
-			{ email: email }, 
-			{ _id : 1, shippingStatus : 1, purchase: 1, createdAt: 1 }
+		const orderHistory = await Order.find(
+			{ email: email },
+			{ _id: 1, shippingStatus: 1, purchase: 1, createdAt: 1 }
 		);
 
-        return orderHistory;
-    },
+		return orderHistory;
+	},
+
+	// [회원] 주문 시 배송지 추가
+	addAddress: async (email, addressInformation) => {
+		await User.updateOne(
+			{ email: email },
+			{ $push: { addressInformation: addressInformation } }
+		);
+	},
 
 	// [회원 비회원 공통] 주문 상세 조회
 	checkOrderDetail: async orderId => {
-		const orderDetails = await Order.findOne({ _id: orderId }, { _id: 1, password: 0 });
+		const orderDetails = await Order.findOne(
+			{ _id: orderId },
+			{ _id: 1, password: 0 }
+		);
 
 		return orderDetails;
 	},
+	guestCheckOrderDetail: async (orderId, password) => {
+		const orderDetails = await Order.findOne(
+			{ _id: orderId, password: password },
+			{ _id: 1, password: 1 }
+		);
 
-    // [회원] 주문 시 배송지 추가
-    addAddress: async (email, addressInformation) => {
-		await User.updateOne({ email: email }, { $push: { addressInformation: addressInformation }});
-    },
+		return orderDetails;
+	},
 };
 
 module.exports = OrderService;
