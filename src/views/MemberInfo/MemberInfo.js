46982eb34a9ec7d35a22015271c73f7276ba4019
[SW5] 이슬 <suerish.e@gmail.com>
2023-07-13 19:10:54 +0900

    Feat: 회원정보 수정 기능 추가

--- src/views/MemberInfo/MemberInfo.js
+++ src/views/MemberInfo/MemberInfo.js
@@ -43,66 +43,23 @@ fetch('/api/users/myPage', {
 	});
 
 modifyBotton.addEventListener('click', () => {
-	console.log(receiverNameInput.value, receiverPasswordInput.value);
-	fetch('/api/users/', {
+	fetch('/api/users', {
 		method: 'PATCH',
 		headers: {
+			'Content-Type': 'application/json',
 			Authorization: hasToken,
 		},
 		body: JSON.stringify({
-			email: receiverNameInput.value,
+			email: receiverEmailInput.value,
 			password: receiverPasswordInput.value,
-			address: [],
+			address: '',
 		}),
-	}).then(res => console.log(res));
+	})
+		.then(res => res.json())
+		.catch(err => alert(err))
+		.then(json => {
+			// console.log(json);
+			alert(json.message);
+		})
+		.catch(err => alert(err));
 });
-
-// //회원 탈퇴로 이동
-// const memberOut = document.querySelectorAll('.member-out');
-
-// memberOut.forEach(data => {
-// 	data.addEventListener('click', () => {
-// 		window.location.href = '/memberDelete'; // 회원 탈퇴 페이지 URL로 이동
-// 	});
-// });
-
-// const modifyButton = document.querySelectorAll('.modify-botton');
-
-// modifyButton.addEventListener('click', () => {
-// 	const newName = receiverNameInput.value;
-// 	const newEmail = receiverEmailInput.value;
-// 	const newPassword = receiverPasswordInput.value;
-// 	const newAddress = receiverAddressInput.value;
-
-// 	// 회원 정보 업데이트 API 호출
-// 	Api.modifyButton(newName, newEmail, newPassword, newAddress)
-// 		.then(response => {
-// 			// 성공적으로 업데이트되었을 때 처리할 코드 작성
-// 			console.log('회원 정보가 성공적으로 업데이트되었습니다.');
-// 			// 예를 들어, 업데이트 성공 메시지를 표시하거나 다른 동작을 수행할 수 있습니다.
-// 		})
-// 		.catch(error => {
-// 			// 업데이트 실패 시 처리할 코드 작성
-// 			console.error('회원 정보 업데이트에 실패했습니다:', error);
-// 			// 예를 들어, 실패 메시지를 표시하거나 오류 처리를 수행할 수 있습니다.
-// 			// 초기값과 다를 경우 api 요청에 사용할 data 객체에 넣어줌
-// 			if (newName !== userData.fullName) {
-// 				data.fullName = newName;
-// 			}
-
-// 			if (newPassword !== userData.password) {
-// 				data.password = newPassword;
-// 			}
-// 			if (isAddressChanged) {
-// 				data.address = {
-// 					postalCode: newPostalCode,
-// 					address1: newAddress1,
-// 					address2: newAddress2,
-// 				};
-// 			}
-
-// 			if (newPhoneNumber && newPhoneNumber !== userData.phoneNumber) {
-// 				data.phoneNumber = newPhoneNumber;
-// 			}
-// 		});
-// });
