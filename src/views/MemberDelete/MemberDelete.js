b7be6728c92573787b3e391edc0e81569a0eb985
[SW5] 이슬 <suerish.e@gmail.com>
2023-07-13 20:42:22 +0900

    Feat: 회원탈퇴 기능 추가

--- src/views/MemberDelete/MemberDelete.js
+++ src/views/MemberDelete/MemberDelete.js
@@ -1,87 +1,80 @@
 import { main } from '/Common/index.js';
 await main();
 
-// const emailElement = document.querySelector('.member-email2');
-// const inputElement = document.querySelector('.input');
-// const checkButton = document.querySelector('.check-button');
+// 브라우저 쿠키에 토큰이 있는지 확인
+function checkJWTTokenInCookie() {
+	const cookies = document.cookie.split(';'); // 모든 쿠키 가져오기
+	for (let i = 0; i < cookies.length; i++) {
+		const cookie = cookies[i].trim();
+		// JWT 토큰 쿠키인지 확인
+		if (cookie.startsWith('uesetToken=')) {
+			const jwtToken = cookie.split('=')[1]; // JWT 토큰 값 가져오기
+			// 토큰이 유효한지 여부 확인
+			if (jwtToken) {
+				return jwtToken; // 유효한 토큰이 존재함
+			}
+		}
+	}
+	return false; // 토큰이 없거나 유효하지 않음
+}
 
-// const email = '디코딩 토큰값.email';
+// 확인된 토큰을 부르는 이름
+const hasToken = checkJWTTokenInCookie();
 
-// emailElement.textContent = email;
-// // 이메일 불러오기
-// // const savedEmail = getEmailFromLocalStorage();
-// // if (savedEmail) {
-// // 	emailElement.textContent = savedEmail;
-// // }
+const base64Url = hasToken.split('.')[1];
+const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+const jsonPayload = decodeURIComponent(
+	window
+		.atob(base64)
+		.split('')
+		.map(function (c) {
+			return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+		})
+		.join('')
+);
 
-// // 확인 버튼 클릭 이벤트 처리
-// checkButton.addEventListener('click', () => {
-// 	console.log('clickCheck');
-// 	const withdrawalStatement = inputElement.value.trim();
+const tokenData = JSON.parse(jsonPayload);
 
-// 	if (withdrawalStatement === '파티피플에서 탈퇴하겠습니다.') {
-// 		// 탈퇴 신청 처리
-// 		processWithdrawal();
-// 		// 알림창 표시
-// 		alert('탈퇴신청이 정상적으로 처리되었습니다.');
-// 	} else {
-// 		// 알림창 표시
-// 		alert('입력한 문구가 일치하지 않습니다.');
-// 	}
-// });
+const userEmail = document.querySelector('.member-email2');
+userEmail.innerText = tokenData.email;
 
-// // 로컬 스토리지에서 이메일 가져오기
-// // function getEmailFromLocalStorage() {
-// // 	return localStorage.getItem('email');
-// // }
+const inputElement = document.querySelector('.input');
+const checkButton = document.querySelector('.check-button');
 
-// // 회원 탈퇴 처리
-// function processWithdrawal() {
-// 	// 이 부분에 실제로 탈퇴 처리를 위한 코드를 작성해야 합니다.
-// 	// 서버와의 통신, 데이터베이스 업데이트 등을 수행해야 합니다.
-// 	// 아래는 탈퇴 처리를 위한 예시 코드입니다.
+// 확인 버튼 클릭 이벤트 처리
+checkButton.addEventListener('click', () => {
+	console.log('clickCheck');
+	const withdrawalStatement = inputElement.value.trim();
 
-// 	// 탈퇴할 회원의 이메일 가져오기
+	if (withdrawalStatement === '파티피플에서 탈퇴하겠습니다.') {
+		// 탈퇴 신청 처리
+		processWithdrawal();
+		// 알림창 표시
+		alert('탈퇴신청이 정상적으로 처리되었습니다.');
+	} else {
+		// 알림창 표시
+		alert('입력한 문구가 일치하지 않습니다.');
+	}
+});
 
-// 	// 서버로 탈퇴 요청 보내기 (예시: fetch를 사용한 POST 요청)
-// 	fetch('/api/users', {
-// 		method: 'DELETE',
-// 		headers: {
-// 			'Content-Type': 'application/json',
-// 			// Authorization: '토큰값',
-// 		},
-// 	})
-// 		.then(response => {
-// 			console.log('response', response);
-// 			return response.json();
-// 		})
-// 		.then(data => {
-// 			// 탈퇴 요청에 대한 처리 결과를 받아와서 여기서 추가적인 작업을 수행할 수 있습니다.
-// 			// 예시: 탈퇴 성공 여부에 따라 다른 동작 수행
-// 			// if (data.success) {
-// 			// 	// 탈퇴 성공 처리
-// 			// 	showSuccessMessage();
-// 			// } else {
-// 			// 	// 탈퇴 실패 처리
-// 			// 	showFailureMessage(data.error);
-// 			// }
-// 			console.log(data);
-// 		})
-// 		.catch(error => {
-// 			// 요청 실패 처리
-// 			console.error('탈퇴 요청에 실패했습니다.', error);
-// 		});
-// }
-
-// // 탈퇴 성공 시 메시지 표시
-// function showSuccessMessage() {
-// 	// 예시: 탈퇴 성공한 경우에는 페이지를 다른 곳으로 리디렉션하거나, 메시지를 출력하는 등의 동작 수행
-// 	// 여기서는 페이지를 리로드하는 예시를 보여줍니다.
-// 	location.reload(); //redirect*
-// }
-
-// // 탈퇴 실패 시 메시지 표시
-// function showFailureMessage(error) {
-// 	// 예시: 탈퇴 실패한 경우에는 실패 메시지를 사용자에게 표시
-// 	alert('탈퇴 처리에 실패했습니다. 에러 메시지: ' + error);
-// }
+// 회원 탈퇴 처리
+function processWithdrawal() {
+	fetch('/api/users', {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: hasToken,
+		},
+	})
+		.then(response => {
+			console.log('response', response);
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error('탈퇴 요청에 실패했습니다.');
+			}
+		})
+		.catch(err => {
+			alert(err);
+		});
+}
--- src/views/MemberInfo/MemberInfo.js
+++ src/views/MemberInfo/MemberInfo.js
@@ -1,6 +1,7 @@
 import { main } from '/Common/index.js';
 await main();
 
+// 브라우저 쿠키에 토큰이 있는지 확인
 function checkJWTTokenInCookie() {
 	const cookies = document.cookie.split(';'); // 모든 쿠키 가져오기
 	for (let i = 0; i < cookies.length; i++) {
@@ -17,16 +18,19 @@ function checkJWTTokenInCookie() {
 	return false; // 토큰이 없거나 유효하지 않음
 }
 
+// 확인된 토큰을 부르는 이름
 const hasToken = checkJWTTokenInCookie();
 // console.log(hasToken);
 
+// 인풋 태그들
 const receiverNameInput = document.querySelector('#receiver-name');
 const receiverEmailInput = document.querySelector('#receiver-email');
 const receiverPasswordInput = document.querySelector('#receiver-password');
 const receiverAddressInput = document.querySelector('#receiver-address');
-
+// 회원정보수정하는 버튼
 const modifyBotton = document.querySelector('.modify-botton');
 
+// 확인된 토큰으로 서버에게 요청해서 현재 유저 정보받아오기
 fetch('/api/users/myPage', {
 	method: 'GET',
 	headers: {
@@ -35,6 +39,7 @@ fetch('/api/users/myPage', {
 })
 	.then(res => res.json())
 	.then(json => {
+		// 받아온 정보들을 위에 태그값에 넣어서 화면에 보여주기
 		receiverNameInput.value = json.userInformation.name;
 		receiverEmailInput.value = json.userInformation.email;
 		receiverPasswordInput.value = json.userInformation.password;
@@ -42,7 +47,9 @@ fetch('/api/users/myPage', {
 		// console.log(json.userInformation);
 	});
 
+// 회원정보수정 버튼을 클릭했을 떄
 modifyBotton.addEventListener('click', () => {
+	// 확인된 토큰으로 서버에게 수정 요청보내기
 	fetch('/api/users', {
 		method: 'PATCH',
 		headers: {
