fc6f9f19c99977f2ae7a27febf20725dbcdbd0f5
[SW5] 이슬 <suerish.e@gmail.com>
2023-07-13 16:42:33 +0900

    Design: Home 화면

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,49 +1,45 @@
 import { main } from '/Common/index.js';
 await main();
 
-const total = document.querySelector('.icons.total');
-const best = document.querySelector('.icons.best');
+const total = document.querySelector('.products.total');
+const best = document.querySelector('.products.best');
 
-// window.addEventListener('DOMContentLoaded', () => {
-// fetch('/api/products', {
-// 	method: 'GET',
-// 	headers: {
-// 		'Content-Type': 'application/json',
-// 	},
-// })
-// 	.then(res => {
-// 		console.log(res);
-// 		if (res.ok) {
-// 			return res.json();
-// 			// 로그인 페이지 이동
-// 		} else {
-// 			throw new Error('조회 실패');
-// 		}
-// 	})
-// 	.catch(err => {
-// 		alert(err);
-// 	})
-// 	.then(({ bestProducts, totalProducts }) => {
-// 		console.log('bestProducts', bestProducts);
-// 		console.log('totalProducts', totalProducts);
-// 		best.innerHTML = bestProducts.map(getProducts).join('');
-// 		total.innerHTML = totalProducts.map(getProducts).join('');
-// 	})
-// 	.catch(err => console.log(err));
-// });
+fetch('/api/products', {
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+})
+	.then(res => {
+		console.log(res);
+		if (res.ok) {
+			return res.json();
+			// 로그인 페이지 이동
+		} else {
+			throw new Error('조회 실패');
+		}
+	})
+	.catch(err => {
+		alert(err);
+	})
+	.then(({ bestProducts, totalProducts }) => {
+		console.log('bestProducts', bestProducts);
+		console.log('totalProducts', totalProducts);
+		best.innerHTML = bestProducts.map(getProducts).join('');
+		total.innerHTML = totalProducts.map(getProducts).join('');
+	})
+	.catch(err => console.log(err));
 
 //상품상세 불러오기
 const getProducts = newProduct => {
 	// ${newProduct.imageURL}
 	return `<li>
-    <a class='icon-img'
+    <a class='product-link'
     href='/products?productId=${newProduct._id}' target='_self' id='bring-list'>
-	<div class="icon-img">
-    <img class='icon-img'
+    <img class="product-img"
     src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE2O8ul5WH3_NdITW8bd4_7gpd6CoSVo1qUY2IHTfE8A&s' alt="product-item"/>
-	</div>
-    <div class="contents1-blod">${newProduct.title}</div>
-    <div class='contents3'>${newProduct.price.toLocaleString()} 원</div>
+    <div class="product-title">${newProduct.title}</div>
+    <div class='product-price'>${newProduct.price.toLocaleString()} 원</div>
     </div>
     </a>
     </li>`;
