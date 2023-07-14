ecee7227ee6158069068a178c1fdcdd40dc4635a
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-14 18:45:01 +0900

    Feat: 관리자 페이지 배송 상태 변경 이벤트 추가

--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,78 +1,29 @@
 import { main } from '/Common/index.js';
 await main();
 
-const testValue = [
-	{
-		_id: 1,
-		title: 'title1',
-		imageURL:
-			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
-		orderAmount: 10,
-		price: 10000,
-		shippingStatus: '배송 중',
-	},
-	{
-		_id: 2,
-		title: 'title2',
-		imageURL:
-			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
-		orderAmount: 10,
-		price: 10000,
-		shippingStatus: '상품 준비 중',
-	},
-	{
-		_id: 3,
-		title: 'title3',
-		imageURL:
-			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
-		orderAmount: 10,
-		price: 10000,
-		shippingStatus: '배송 중',
-	},
-	{
-		_id: 4,
-		title: 'title4',
-		imageURL:
-			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
-		orderAmount: 10,
-		price: 10000,
-		shippingStatus: '배송 완료',
-	},
-];
 const getOrders = newOrders => {
-	// ${new Date(newOrders.createdAt).toLocaleString()}
-	// newOrders._id
-	// const shippingStat = ['상품 준비 중', '배송 중', '배송 완료'];
 	return `
 	<li>
 	<article>
 		<div class="info">
 			<div>
-				<span class="date">2023-01-01</span>
+				<span class="date">${new Date(newOrders.createdAt).toLocaleString()}</span>
 				<span class="status">${newOrders.shippingStatus}</span>
 			</div>
-			<select name="${newOrders._id}">
-			<option value = '상품 준비 중' ${
-				newOrders.shippingStatus === '상품 준비 중' ? 'selected' : ''
-			}>상품 준비 중</option>
-			<option value = '배송 중' ${
-				newOrders.shippingStatus === '배송 중' ? 'selected' : ''
-			}>배송 중</option>
-			<option value = '배송 완료' ${
-				newOrders.shippingStatus === '배송 완료' ? 'selected' : ''
-			}>배송 완료</option>
-			</select>
-			
+			<a
+			href="/admin/orderId/?orderId=${newOrders._id}"
+			class="detail-btn"
+			>주문 상세보기 ></a>
 		</div>
 		<ul class="products-list">
 	<li>
 		<div class="thumbnail">
-			<img src="${newOrders.imageURL}" />
-			<span class="title">${newOrders.title}</span>
+			<img src="${newOrders.purchase[0].imageURL[0]}" />
+			<span class="title">${newOrders.purchase[0].title}</span>
 		</div>
 		<div><span>${
-			newOrders.orderAmount
-		}</span> 개 &#215; <span>${newOrders.price.toLocaleString()}</span>원</div>
+			newOrders.purchase[0].orderAmount
+		}</span> 개 &#215; <span>${newOrders.purchase[0].price.toLocaleString()}</span>원</div>
 	</li>	
 	</ul>
 	</article>
@@ -98,32 +49,46 @@ function checkJWTTokenInCookie() {
 
 // 확인된 토큰을 부르는 이름
 const hasToken = checkJWTTokenInCookie();
-// console.log(hasToken);
 
-const userName = document.querySelector('#user-name');
-const userRole = document.querySelector('.user-role');
+if (hasToken) {
+	console.log('JWT 토큰이 쿠키에 존재합니다.');
+
+	const base64Url = hasToken.split('.')[1];
+	const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	const jsonPayload = decodeURIComponent(
+		window
+			.atob(base64)
+			.split('')
+			.map(function (c) {
+				return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+			})
+			.join('')
+	);
+	const tokenData = JSON.parse(jsonPayload);
+	document.querySelector('#user-name').innerText = tokenData.name;
+} else {
+	console.log('JWT 토큰이 쿠키에 존재하지 않습니다.');
+}
+
 const itemsList = document.querySelector('.history-list');
 
 // 확인된 토큰으로 서버에게 요청해서 현재 유저 정보받아오기
-fetch('/api/users/myPage', {
+fetch(`/api/admin`, {
 	method: 'GET',
 	headers: {
+		'Content-Type': 'application/json',
 		Authorization: hasToken,
 	},
 })
 	.then(res => res.json())
 	.then(json => {
 		// 받아온 정보들을 위에 태그값에 넣어서 화면에 보여주기
-		console.log('json', json);
-		const { name, role } = json.userInformation;
+		const { orders } = json;
 
-		userName.innerText = name;
-		userRole.innerText = role;
-
-		console.log(json.userInformation);
+		if (orders.length <= 0) {
+			itemsList.innerHTML =
+				'<li style="padding:20px">주문 내역이 없습니다.</li>';
+		} else {
+			itemsList.innerHTML = orders.map(getOrders);
+		}
 	});
-if (false) {
-	itemsList.innerHTML = '<li style="padding:20px">주문 내역이 없습니다.</li>';
-} else {
-	itemsList.innerHTML = testValue.map(getOrders);
-}
