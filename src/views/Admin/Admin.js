a7b4ae475d0986ccce59c47ac06a57c4ef2755bd
[SW5] 최재혁 <cslzh4433@gmail.com>
2023-07-14 16:33:57 +0900

    Feat: admin페이지 주문 목록 생성

--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,6 +1,85 @@
 import { main } from '/Common/index.js';
 await main();
 
+const testValue = [
+	{
+		_id: 1,
+		title: 'title1',
+		imageURL:
+			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
+		orderAmount: 10,
+		price: 10000,
+		shippingStatus: '배송 중',
+	},
+	{
+		_id: 2,
+		title: 'title2',
+		imageURL:
+			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
+		orderAmount: 10,
+		price: 10000,
+		shippingStatus: '상품 준비 중',
+	},
+	{
+		_id: 3,
+		title: 'title3',
+		imageURL:
+			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
+		orderAmount: 10,
+		price: 10000,
+		shippingStatus: '배송 중',
+	},
+	{
+		_id: 4,
+		title: 'title4',
+		imageURL:
+			'https://partyhae.com/web/product/big/202209/98d6274f8937871c917a6d547d0351b1.jpg',
+		orderAmount: 10,
+		price: 10000,
+		shippingStatus: '배송 완료',
+	},
+];
+const getOrders = newOrders => {
+	// ${new Date(newOrders.createdAt).toLocaleString()}
+	// newOrders._id
+	// const shippingStat = ['상품 준비 중', '배송 중', '배송 완료'];
+	return `
+	<li>
+	<article>
+		<div class="info">
+			<div>
+				<span class="date">2023-01-01</span>
+				<span class="status">${newOrders.shippingStatus}</span>
+			</div>
+			<select name="${newOrders._id}">
+			<option value = '상품 준비 중' ${
+				newOrders.shippingStatus === '상품 준비 중' ? 'selected' : ''
+			}>상품 준비 중</option>
+			<option value = '배송 중' ${
+				newOrders.shippingStatus === '배송 중' ? 'selected' : ''
+			}>배송 중</option>
+			<option value = '배송 완료' ${
+				newOrders.shippingStatus === '배송 완료' ? 'selected' : ''
+			}>배송 완료</option>
+			</select>
+			
+		</div>
+		<ul class="products-list">
+	<li>
+		<div class="thumbnail">
+			<img src="${newOrders.imageURL}" />
+			<span class="title">${newOrders.title}</span>
+		</div>
+		<div><span>${
+			newOrders.orderAmount
+		}</span> 개 &#215; <span>${newOrders.price.toLocaleString()}</span>원</div>
+	</li>	
+	</ul>
+	</article>
+</li>
+	`;
+};
+
 // 브라우저 쿠키에 토큰이 있는지 확인
 function checkJWTTokenInCookie() {
 	const cookies = document.cookie.split(';'); // 모든 쿠키 가져오기
@@ -23,6 +102,7 @@ const hasToken = checkJWTTokenInCookie();
 
 const userName = document.querySelector('#user-name');
 const userRole = document.querySelector('.user-role');
+const itemsList = document.querySelector('.history-list');
 
 // 확인된 토큰으로 서버에게 요청해서 현재 유저 정보받아오기
 fetch('/api/users/myPage', {
@@ -35,10 +115,15 @@ fetch('/api/users/myPage', {
 	.then(json => {
 		// 받아온 정보들을 위에 태그값에 넣어서 화면에 보여주기
 		console.log('json', json);
-		const { name, email, password, role } = json.userInformation;
+		const { name, role } = json.userInformation;
 
 		userName.innerText = name;
 		userRole.innerText = role;
 
 		console.log(json.userInformation);
 	});
+if (false) {
+	itemsList.innerHTML = '<li style="padding:20px">주문 내역이 없습니다.</li>';
+} else {
+	itemsList.innerHTML = testValue.map(getOrders);
+}
